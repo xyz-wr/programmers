508b38397c6aa99b2b2eaff9d2dd7c0a66683153
xyz-wr <pponi13468@naver.com>
2022-09-14 21:35:02 +0900

    level1

---
+++ level1/두 정수 사이의 합.js
@@ -0,0 +1,15 @@
+function solution(a, b) {
+    let sum = 0;
+    if (a === b) {
+        return a;
+    } else if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = b; i <= a; i++) {
+            sum += i;
+        }
+    }
+    return sum;
+}
